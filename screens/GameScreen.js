89ebc2af9dd83812e1bac60e3f31a96386f42fdf
rktdnjs <rktdnjs@naver.com>
2022-11-06 23:17:44 +0900

    65 save

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,9 @@
 // 실질적으로 게임 진행과 관련된 컴포넌트들
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import NumberContainer from "../components/game/NumberContainer";
 import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 // 따로 외부에서 가지고 온 코드임, 그냥 1~99 난수 생성기
 function generateRandomBetween(min, max, exclude) {
@@ -15,9 +16,30 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 function GameScreen({ userNumber }) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if ((direction === 'lower' && currentGuess < userNumber) || 
+    (direction === 'greater' && currentGuess > userNumber)) {
+        Alert.alert("거짓말하지 마세요!!", "잘 못 말씀하신 것 같습니다..", [
+            { text : 'Sorry!', style : 'cancel'}
+        ])
+        return;
+    }
+    if (direction === "lower") { // 작은 숫자를 추측하세요!
+        maxBoundary = currentGuess;
+    }
+    else {
+        minBoundary = currentGuess + 1; // 이런 코드들을 자세히 이해하려고 하진 않아도 됨. 느낌만!
+    }
+    const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+    setCurrentGuess(newRndNumber)
+}
 
   return (
     <View style={styles.screen}>
@@ -25,7 +47,10 @@ function GameScreen({ userNumber }) {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher OR Lower?</Text>
-        {/* + - */}
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+        </View>
       </View>
       <View></View>
     </View>
