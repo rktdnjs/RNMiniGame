83e7d727d3daa79daf21ff660c2d9aa3c863de73
rktdnjs <rktdnjs@naver.com>
2022-11-06 21:36:27 +0900

    57-58 User Input Handling & Alert

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -2,15 +2,17 @@
 // 버튼을 만들 때 반드시 이해해야 하는 것은 핵심 컴포넌트들을 합치는 것
 import { Text, View, Pressable, StyleSheet } from 'react-native';
 
-function PrimaryButton(props) {
+function PrimaryButton({children, onPress}) {
 
 
     return (
         <View style={styles.buttonOuterContainer}>
             <Pressable
                 style={styles.buttonInnerContainer}
-                android_ripple={{ color: '#640233' }}>
-                <Text style={styles.buttonText}>{props.children}</Text>
+                android_ripple={{ color: '#640233' }}
+                onPress={onPress}
+                >
+                <Text style={styles.buttonText}>{children}</Text>
             </Pressable>
         </View >
     )
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,48 @@
 // 시작 화면 역할을 할 컴포넌트가 들어감
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState('');
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber('')
+  }
+
+  function confirmInputHanlder() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99)
+    {
+      Alert.alert(
+        '유효하지 않은 숫자입니다!', 
+        '숫자는 1이상 99이하여야 합니다.',
+        [{text:'Okay', style:'destructive', onPress: resetInputHandler}]);
+      return;
+    }
+    console.log("유효한 숫자입니다!");
+  }
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
         style={styles.numberInput}
         maxLength={2}
         keyboardType="number-pad"
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHanlder}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
