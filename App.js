64ac4047e82965698261cc14f846524864b071a0
rktdnjs <rktdnjs@naver.com>
2022-11-11 15:06:18 +0900

    73 save

--- App.js
+++ App.js
@@ -10,14 +10,15 @@ import AppLoading from "expo-app-loading";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const [fontsLoaded] = useFonts({
-    'open-sans' : require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold' : require('./assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
 
   if (!fontsLoaded) {
-    return <AppLoading/>
+    return <AppLoading />
   }
 
   function pickedNumberHandler(pickedNumber) {
@@ -29,14 +30,22 @@ export default function App() {
     setGameIsOver(true);
   }
 
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
   }
 
   if (gameIsOver && userNumber) {
-    screen = <GameOverScreen/>
+    screen = <GameOverScreen
+      userNumber={userNumber}
+      roundsNumber={guessRounds}
+      onStartNewGame={startNewGameHandler} />
   }
 
   return (
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,9 @@
 // 게임이 끝나면 보여질 화면
 import { Text, View, Image, StyleSheet } from "react-native";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
 
-function GameOverScreen() {
+function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
   return (
     <View style={styles.rootContainer}>
       <Title>Game Over!</Title>
@@ -10,8 +11,12 @@ function GameOverScreen() {
         <Image style={styles.image} source={require('../assets/images/success.png')} />
       </View>
       <View>
-        <Text>당신의 핸드폰은 숫자를 맞히기 위해 X번의 기회를 썼습니다.</Text>
+        <Text style={styles.summaryText}>
+          당신의 핸드폰은 숫자 <Text style={styles.highlight}>{userNumber}</Text>를 맞히기 위해 
+          <Text style={styles.highlight}>{roundsNumber}</Text>번의 기회를 썼습니다.
+        </Text>
       </View>
+      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
   )
 }
@@ -19,23 +24,33 @@ function GameOverScreen() {
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
-  rootContainer : {
-    flex : 1,
-    padding : 24,
-    justifyContent : 'center',
-    alignItems : 'center'
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  imageContainer : {
-    width : 300,
-    height : 300,
-    borderRadius : 150,
-    borderWidth : 3, // 경계선
-    borderColor : '#3b021f',
-    overflow : 'hidden', // 이미지의 사각형 모서리를 숨김
-    margin : 36
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3, // 경계선
+    borderColor: '#3b021f',
+    overflow: 'hidden', // 이미지의 사각형 모서리를 숨김
+    margin: 36
   },
   image: {
-    width :'100%', // %값을 설정한 요소의 컨테이너에 비례함
-    height : '100%'
+    width: '100%', // %값을 설정한 요소의 컨테이너에 비례함
+    height: '100%'
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize : 24,
+    textAlign : 'center',
+    marginBottom : 24
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: '#72063c'
   }
 })
